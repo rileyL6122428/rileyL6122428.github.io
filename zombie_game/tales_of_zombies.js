d57a12314b8f71a38facd37549dc3134dc5e479c
Riley Littlefield <rileylittlefield@ymail.com>
2016-06-29 11:53:25 -0400

    adds level unlocking to zombie game

--- zombie_game/tales_of_zombies.js
+++ zombie_game/tales_of_zombies.js
@@ -3,7 +3,7 @@ var GameView = require('./lib/game_view');
 
 var canvasEl = document.getElementById("game-canvas");
 var ctx = canvasEl.getContext('2d');
-ctx.canvas.height = 650;
+ctx.canvas.height = 750;
 ctx.canvas.width  = 1192;
 
 var gameView = new GameView(ctx, canvasEl.width, canvasEl.height);
